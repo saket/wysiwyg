c30a54078bee7ade1492460627809d075ec24340
Saket Narayan <saket@uncommon.is>
2018-11-01 10:51:41 +0530

    Migrate to AndroidX

--- app/src/main/java/me/saket/baseandroidproject/MainActivity.kt
+++ app/src/main/java/me/saket/baseandroidproject/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.saket.baseandroidproject
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
