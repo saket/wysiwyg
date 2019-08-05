f81c6229a5040943919c95791a87f40df9b82aff
Saket Narayan <saket@squareup.com>
2019-08-05 10:43:51 -0400

    Remove dependency on appcompat

--- renderer/src/main/java/me/saket/markdownrenderer/WysiwygTheme.kt
+++ renderer/src/main/java/me/saket/markdownrenderer/WysiwygTheme.kt
@@ -1,9 +1,9 @@
 package me.saket.markdownrenderer
 
 import android.content.Context
+import android.os.Build
 import androidx.annotation.ColorInt
 import androidx.annotation.Px
-import androidx.core.content.ContextCompat
 import ru.noties.markwon.core.MarkwonTheme
 
 /**
@@ -11,50 +11,65 @@ import ru.noties.markwon.core.MarkwonTheme
  */
 data class WysiwygTheme(
 
-    /** Used for resolving default colors and dimensions in [MarkwonTheme.builderWithDefaults]. */
-    val context: Context,
+  /** Used for resolving default colors and dimensions in [MarkwonTheme.builderWithDefaults]. */
+  val context: Context,
 
-    /** Color used for highlighting '**', '~~' and other syntax characters. */
-    @ColorInt
-    val syntaxColor: Int = color(context, R.color.markdown_syntax),
+  /** Color used for highlighting '**', '~~' and other syntax characters. */
+  @ColorInt
+  val syntaxColor: Int = color(context, R.color.markdown_syntax),
 
-    @ColorInt
-    val blockQuoteIndentationRuleColor: Int = color(context, R.color.markdown_blockquote_indentation_rule),
+  @ColorInt
+  val blockQuoteIndentationRuleColor: Int = color(
+      context, R.color.markdown_blockquote_indentation_rule
+  ),
 
-    @ColorInt
-    val blockQuoteTextColor: Int = color(context, R.color.markdown_blockquote_text),
+  @ColorInt
+  val blockQuoteTextColor: Int = color(context, R.color.markdown_blockquote_text),
 
-    /** Width of a block-quote's vertical line/stripe/rule. */
-    @Px
-    val blockQuoteVerticalRuleStrokeWidth: Int = dimensPx(context, R.dimen.markdown_blockquote_vertical_rule_stroke_width),
+  /** Width of a block-quote's vertical line/stripe/rule. */
+  @Px
+  val blockQuoteVerticalRuleStrokeWidth: Int = dimensPx(
+      context, R.dimen.markdown_blockquote_vertical_rule_stroke_width
+  ),
 
-    /** Gap before a block of ordered/unordered list. */
-    @Px
-    val listBlockIndentationMargin: Int = dimensPx(context, R.dimen.markdown_text_block_indentation_margin),
+  /** Gap before a block of ordered/unordered list. */
+  @Px
+  val listBlockIndentationMargin: Int = dimensPx(
+      context, R.dimen.markdown_text_block_indentation_margin
+  ),
 
-    @ColorInt
-    val linkUrlColor: Int = color(context, R.color.markdown_link_url),
+  @ColorInt
+  val linkUrlColor: Int = color(context, R.color.markdown_link_url),
 
-    @ColorInt
-    val linkTextColor: Int = color(context, R.color.markdown_link_text),
+  @ColorInt
+  val linkTextColor: Int = color(context, R.color.markdown_link_text),
 
-    @ColorInt
-    val thematicBreakColor: Int = color(context, R.color.markdown_thematic_break),
+  @ColorInt
+  val thematicBreakColor: Int = color(context, R.color.markdown_thematic_break),
 
-    @Px
-    val thematicBreakThickness: Int = dimensPx(context, R.dimen.markdown_thematic_break_thickness),
+  @Px
+  val thematicBreakThickness: Int = dimensPx(context, R.dimen.markdown_thematic_break_thickness),
 
-    @ColorInt
-    val codeBackgroundColor: Int = color(context, R.color.markdown_code_background)
+  @ColorInt
+  val codeBackgroundColor: Int = color(context, R.color.markdown_code_background)
 ) {
-    val markwonTheme = MarkwonTheme.builderWithDefaults(context)
-        .headingBreakHeight(0)
-        .blockQuoteColor(blockQuoteIndentationRuleColor)
-        .blockQuoteWidth(blockQuoteVerticalRuleStrokeWidth)
-        .blockMargin(listBlockIndentationMargin)
-        .codeBackgroundColor(codeBackgroundColor)
-        .build()
+  val markwonTheme = MarkwonTheme.builderWithDefaults(context)
+      .headingBreakHeight(0)
+      .blockQuoteColor(blockQuoteIndentationRuleColor)
+      .blockQuoteWidth(blockQuoteVerticalRuleStrokeWidth)
+      .blockMargin(listBlockIndentationMargin)
+      .codeBackgroundColor(codeBackgroundColor)
+      .build()
 }
 
-val color = { context: Context, colorResId: Int -> ContextCompat.getColor(context, colorResId) }
-val dimensPx = { context: Context, dimenResId: Int -> context.resources.getDimensionPixelSize(dimenResId) }
+@Suppress("DEPRECATION")
+private val color = { context: Context, colorResId: Int ->
+  when {
+    Build.VERSION.SDK_INT >= 23 -> context.resources.getColor(colorResId, context.theme)
+    else -> context.resources.getColor(colorResId)
+  }
+}
+
+private val dimensPx = { context: Context, dimenResId: Int ->
+  context.resources.getDimensionPixelSize(dimenResId)
+}
