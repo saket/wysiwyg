8809dc226618ee5eefe8114799a45fa528c8586a
Saket Narayan <saket@saket.me>
2026-04-20 15:44:30 -0700

    Remove flexmark as the default parser arg

--- wysiwyg/src/main/kotlin/me/saket/wysiwyg/Wysiwyg.kt
+++ wysiwyg/src/main/kotlin/me/saket/wysiwyg/Wysiwyg.kt
@@ -8,14 +8,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import me.saket.wysiwyg.format.OnEnterMarkdownFormatters
 import me.saket.wysiwyg.internal.RealWysiwyg
-import me.saket.wysiwyg.parser.flexmark.FlexmarkMarkdownParser
 import me.saket.wysiwyg.parser.MarkdownParser
 
 @Composable
 fun rememberWysiwyg(
   textState: TextFieldState,
   theme: WysiwygTheme,
-  markdownParser: MarkdownParser = FlexmarkMarkdownParser(),
+  markdownParser: MarkdownParser,
   onEnterFormatters: OnEnterMarkdownFormatters = OnEnterMarkdownFormatters.Default,
 ): Wysiwyg {
   val wysiwyg = remember(textState, theme, markdownParser, onEnterFormatters) {
