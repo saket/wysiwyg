3ae07fe794b0947293c8a6ac041dbcd0fa4a70b6
Saket Narayan <saket@saket.me>
2026-05-11 10:43:02 -0700

    Update demo text

--- sample/src/main/kotlin/me/saket/wysiwyg/sample/WysiwygEditor.kt
+++ sample/src/main/kotlin/me/saket/wysiwyg/sample/WysiwygEditor.kt
@@ -40,7 +40,7 @@ fun WysiwygEditor(
         |Markdown is a **lightweight** and easy-to-use `syntax` for styling all forms of ~~web~~ writing.
         |> The overriding design goal for Markdown's formatting syntax is to make it as readable as possible.
         |---
-        |Markdown was originally developed by [John Gruber](daringfireball.net/markdown) (with help from Aaron Swartz).
+        |Markdown was originally developed by [John Gruber](daringfireball.net/markdown), with help from Aaron Swartz.
         """.trimMargin(),
   )
 
