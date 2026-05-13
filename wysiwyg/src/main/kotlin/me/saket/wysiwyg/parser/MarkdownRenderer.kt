1e6fc2861dc1b3c7c6472d09c4e94f66b2f6ca91
Saket Narayan <saket@saket.me>
2026-05-13 09:40:35 -0700

    Annotate RenderResult with @Poko

--- wysiwyg/src/main/kotlin/me/saket/wysiwyg/parser/MarkdownRenderer.kt
+++ wysiwyg/src/main/kotlin/me/saket/wysiwyg/parser/MarkdownRenderer.kt
@@ -1,6 +1,7 @@
 package me.saket.wysiwyg.parser
 
 import androidx.compose.foundation.text.input.TextFieldBuffer
+import dev.drewhamilton.poko.Poko
 import me.saket.wysiwyg.MarkdownSpanPainter
 import me.saket.wysiwyg.internal.AnnotatedStringMarkdownRenderer
 import me.saket.wysiwyg.internal.AnnotatedStringMarkdownRenderer as Impl
@@ -20,7 +21,7 @@ fun interface MarkdownRenderer {
 }
 
 // todo: kdoc
-class RenderResult(
+@Poko class RenderResult(
   val spanPainters: List<MarkdownSpanPainter>,
 ) {
   companion object {
