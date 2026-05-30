0eb675f9733ecb4d6c64b0ec3b26f9bb920600f6
Saket Narayan <saket@saket.me>
2026-05-30 14:05:06 -0700

    Update some test text

--- wysiwyg/src/test/kotlin/me/saket/wysiwyg/WysiwygTest.kt
+++ wysiwyg/src/test/kotlin/me/saket/wysiwyg/WysiwygTest.kt
@@ -83,7 +83,7 @@ class WysiwygTest {
           |Markdown is a **lightweight** and easy-to-use `syntax` for styling all forms of ~~web~~ writing.
           |> The overriding design goal for Markdown's formatting syntax is to make it as readable as possible.
           |---
-          |Markdown was originally developed by [John Gruber](daringfireball.net/markdown).
+          |Markdown was originally developed by [John Gruber](daringfireball.net/markdown), with help from Aaron Swartz.
           """.trimMargin(),
           theme = theme.theme,
           textStyle = DefaultTextStyle.copy(color = theme.textColor),
