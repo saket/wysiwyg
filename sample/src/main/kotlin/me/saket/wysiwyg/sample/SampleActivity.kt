f3a1f9376b46afb7cefeb1e80946f1674d361553
Saket Narayan <saket@saket.me>
2026-05-31 00:47:30 -0700

    Sample: Inline WysiwygEditor()

--- sample/src/main/kotlin/me/saket/wysiwyg/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/wysiwyg/sample/SampleActivity.kt
@@ -5,14 +5,34 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fitInside
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.WindowInsetsRulers
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import me.saket.wysiwyg.WsyiwygTextField
+import me.saket.wysiwyg.rememberWysiwyg
 
 class SampleActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +48,61 @@ class SampleActivity : AppCompatActivity() {
         }
       }
     }
+  }
+}
+
+@Composable
+private fun WysiwygEditor(
+  modifier: Modifier = Modifier,
+) {
+  val textState = rememberTextFieldState(
+    initialText =
+      """
+        |# Wysiwyg
+        |
+        |Markdown is a **lightweight** and easy-to-use `syntax` for styling all forms of ~~web~~ writing.
+        |> The overriding design goal for Markdown's formatting syntax is to make it as readable as possible.
+        |---
+        |Markdown was originally developed by [John Gruber](daringfireball.net/markdown), with help from Aaron Swartz.
+        """.trimMargin(),
+  )
+
+  val focusRequester = remember { FocusRequester() }
+  LaunchedEffect(focusRequester) {
+    delay(50) // Workaround for https://issuetracker.google.com/issues/199631318.
+    focusRequester.requestFocus()
+  }
+
+  val wysiwyg = rememberWysiwyg(
+    textState = textState,
+  )
+
+  Column(modifier) {
+    Box(
+      Modifier
+        .fillMaxWidth()
+        .weight(1f)
+        .fitInside(WindowInsetsRulers.SystemBars.current)
+        .fitInside(WindowInsetsRulers.Ime.current),
+    ) {
+      WsyiwygTextField(
+        modifier = Modifier.focusRequester(focusRequester),
+        contentPadding = PaddingValues(16.dp),
+        wysiwyg = wysiwyg,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        textStyle = TextStyle(
+          fontSize = 16.sp,
+          lineHeight = 1.5.em,
+          letterSpacing = 0.5.sp,
+          color = LocalContentColor.current,
+        ),
+      )
+    }
+
+    MarkdownFormattingBar(
+      modifier = Modifier.fillMaxWidth(),
+      state = textState,
+    )
   }
 }
 
